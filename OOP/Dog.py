8530bc1f6f52d5850f88a5a266a49f74640f5324
Bryan Eduria <bryaneduria@vikingcloud.com>
2023-06-27 14:14:20 +0800

    OOP Encapsulation class finished

---
+++ OOP/Dog.py
@@ -0,0 +1,15 @@
+class Dog:
+    # Class attribute
+    species = "Canis familiaris"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # Instance method
+    def description(self):
+        return f"{self.name} is {self.age} years old"
+    
+     # Another instance method
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
---
+++ main.py
@@ -0,0 +1,6 @@
+# Notes - import the class by specifying the file name on the "from" statement
+from OOP.Dog import Dog
+
+husky = Dog("Carla", 1)
+
+print(husky.description())
